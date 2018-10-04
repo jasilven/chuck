69fd46289485eb0b741befc21bd51f6200ab98d2
jasilven <jasilven@gmail.com>
2018-10-04 15:04:04 +0300

    added plotting, naming changes

--- main.rkt
+++ main.rkt
@@ -4,38 +4,56 @@
 (require racket/string
          json
          net/http-client
-         threading)
+         threading
+         plot
+         net/url)
 
-(define (get-request-body host uri)
-  (let-values ([(status headers in) (http-sendrecv host uri)])
+(define (get-request-body host path)
+  (let-values ([(status headers in) (http-sendrecv host path)])
     (read-json in)))
 
-(define (fetch-jokes host uri)
-  (let ([body (get-request-body host uri)])
+(define (fetch-jokes from)
+  (let* ([url (string->url from)]
+         [body (get-request-body (url-host url) (path->string (url->path url)))])
     (map (λ (item) (hash-ref item 'joke))
          (hash-ref body 'value))))
 
-(define (word-counts lines)
+(define (word-counts jokes)
   (let ([words (make-hash)])
-    (for* ([joke lines]
+    (for* ([joke jokes]
            [word (string-split (string-downcase (string-replace joke #px"\\P{^P}" "")))]
            #:when (> (string-length word) 3))
       (let ([key (string->keyword word)])
         (if (hash-has-key? words key)
             (hash-set! words key (+ 1 (hash-ref words key)))
             (hash-set! words key 1))))
-    (sort (hash->list words)
+    (sort (words-hash->list words)
           > #:key cdr)))
+
+(define (words-hash->list words)
+  (map (λ (item)
+         (cons (keyword->string (car item)) (cdr item)))
+       (hash->list words)))
 
 (define (display-word-counts wc)
   (for ([item wc]
         [index (in-range (length wc))])
-    (printf "~a: ~a~n"
-            (+ 1 index)
-            (keyword->string (car item)))))
+    (printf "~a: ~a~n" (+ 1 index) (car item)))
+  wc)
 
-;; Find out 10 most frequently used words found in Chuck Norris jokes
-(~> (fetch-jokes "api.icndb.com" "/jokes")
+(define (pair->vector p)
+  (vector (car p) (cdr p)))
+
+(define (display-word-counts-histogram wc)
+  (let ([vals (map (λ (item) (pair->vector item)) wc)])
+    (parameterize ([plot-width 900]
+                   [plot-x-label "words"]
+                   [plot-y-label "word count"])
+      (plot (list (discrete-histogram vals))))))
+
+;; Show 10 most frequently used words found in Chuck Norris jokes
+(~> (fetch-jokes "http://api.icndb.com/jokes")
     (word-counts)
     (take 10)
-    (display-word-counts))
+    (display-word-counts)
+    (display-word-counts-histogram))
