cc226215acaed031fa5e8809855763866ad5feaf
jasilven <jasilven@gmail.com>
2018-10-06 11:09:55 +0300

    refactored towards more idiomatic racket code

--- main.rkt
+++ main.rkt
@@ -8,52 +8,47 @@
          plot
          net/url)
 
-(define (get-request-body host path)
+(define (get-json-data host path)
   (let-values ([(status headers in) (http-sendrecv host path)])
     (read-json in)))
 
 (define (fetch-jokes from)
   (let* ([url (string->url from)]
-         [body (get-request-body (url-host url) (path->string (url->path url)))])
-    (map (λ (item) (hash-ref item 'joke))
-         (hash-ref body 'value))))
+         [path (path->string (url->path url))]
+         [json (get-json-data (url-host url) path)])
+    (map (λ (j) (hash-ref j 'joke))
+         (hash-ref json 'value))))
 
-(define (word-counts jokes)
-  (let ([words (make-hash)])
-    (for* ([joke jokes]
-           [word (string-split (string-downcase (string-replace joke #px"\\P{^P}" "")))]
-           #:when (> (string-length word) 3))
-      (let ([key (string->keyword word)])
-        (if (hash-has-key? words key)
-            (hash-set! words key (+ 1 (hash-ref words key)))
-            (hash-set! words key 1))))
-    (sort (words-hash->list words)
-          > #:key cdr)))
+(define (strip-punctuation astr)
+  (string-replace astr #px"\\P{^P}" ""))
 
-(define (words-hash->list words)
-  (map (λ (item)
-         (cons (keyword->string (car item)) (cdr item)))
-       (hash->list words)))
+(define (word-frequencies jokes #:sort do-sort)
+  (define words (make-hash))
+  (define (words->list words)
+    (map (λ (w) (cons (keyword->string (car w)) (cdr w)))
+         (hash->list words)))
+  (for* ([joke (map (λ (j) (string-downcase j)) jokes)]
+         [word (string-split (strip-punctuation joke))]
+         #:when (> (string-length word) 3))
+    (hash-update! words (string->keyword word) add1 0))
+  (if do-sort
+      (sort (words->list words) > #:key cdr)
+      (words->list words)))
 
-(define (display-word-counts wc)
-  (for ([item wc]
-        [index (in-range (length wc))])
-    (printf "~a: ~a~n" (+ 1 index) (car item)))
-  wc)
-
-(define (pair->vector p)
-  (vector (car p) (cdr p)))
-
-(define (display-word-counts-histogram wc)
-  (let ([vals (map (λ (item) (pair->vector item)) wc)])
-    (parameterize ([plot-width 900]
-                   [plot-x-label "words"]
-                   [plot-y-label "word count"])
-      (plot (list (discrete-histogram vals))))))
+(define (word-frequencies-histogram freq #:width w)
+  (define (pair->vector p) (vector (car p) (cdr p)))
+  (define vals (map (λ (item) (pair->vector item)) freq))
+  (parameterize ([plot-width w]
+                 [plot-new-window? #t]
+                 [plot-x-label "words"]
+                 [plot-y-label "word count"])
+    (plot (list (discrete-histogram vals)))))
 
 ;; Show 10 most frequently used words found in Chuck Norris jokes
-(~> (fetch-jokes "http://api.icndb.com/jokes")
-    (word-counts)
-    (take 10)
-    (display-word-counts)
-    (display-word-counts-histogram))
+(define (main)
+  (~> (fetch-jokes "http://api.icndb.com/jokes")
+      (word-frequencies #:sort #t)
+      (take 10)
+      (word-frequencies-histogram #:width 900)))
+
+(main)
